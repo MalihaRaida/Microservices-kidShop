169371227a18dd2d586eddd81b1de24fd96c991f
zsristy <zannatunsristy@gmail.com>
2020-12-25 00:57:58 +0600

    productDB

---
+++ ProductServiceDB/productschema.sql
@@ -0,0 +1,12 @@
+drop database if exists productService;
+create database productService;
+use productService;
+
+create table product(
+    id varchar(36) primary key not null,
+    name varchar(100) not null,
+    categoryId varchar(36) not null,
+    categoryName varchar(100) not null ,
+    averageRating float not null default 0,
+    numberOfRaters int not null default 0
+);
