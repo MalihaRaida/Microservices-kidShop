2d9c4c7fe4ddfa1eea59e6389b4a4fae023a27dc
nawshin81 <nawshinulfat@iut-dhaka.edu>
2020-12-25 02:52:05 +0600

    get '/product/list'

--- ProductService/index.js
+++ ProductService/index.js
@@ -1,34 +1,35 @@
-const mysql= require('mysql');
-const express=require('express');
-var app=express();
-const bodyparser=require('body-parser');
-const { request, response } = require('express');
+const mysql = require("mysql");
+const express = require("express");
+var app = express();
+const bodyparser = require("body-parser");
 
-app.use(bodyparser.json)
+app.use(bodyparser.json());
 
-var mysqlConnection= mysql.createConnection({
-    host:'localhost',
-    user:'user',
-    password:'1234',
-    database:'productService',
+var mysqlConnection = mysql.createConnection({
+  host: "localhost",
+  user: "user",
+  password: "1234",
+  database: "productService",
 });
 
-mysqlConnection.connect((error)=>{
-    if(!error){
-        console.log('Succeded');
-    }else{
-        console.log('Failed');
+mysqlConnection.connect((error) => {
+  if (!error) {
+    console.log("Succeded");
+  } else {
+    console.log("Failed");
+  }
+});
+
+app.listen(3005, () => console.log("Express server at port 3005"));
+
+app.get("/product/list", (request,response) => {
+  console.log("hii");
+  mysqlConnection.query("SELECT * from product", (error, rows, field) => {
+    if (!error) {
+      console.log(rows);
+      response.send(rows);
+    } else {
+      console.log(error);
     }
-})
-
-app.listen(3005,()=>console.log('Express server at port 3005'));
-
-app.get('/product/list',(request,response)=>{
-    mysqlConnection.query('SELECT * from product',(error,rows,field)=>{
-       if(!error) {
-           response.send(rows);
-       }else{
-           console.log(error);
-       }
-    })
-})
+  });
+});
