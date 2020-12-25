ae9b6df72ca4ecadeb32cdc44d492806689cb1f4
nawshin81 <nawshinulfat@iut-dhaka.edu>
2020-12-26 04:11:15 +0600

    update product category

--- ProductService/index.js
+++ ProductService/index.js
@@ -10,7 +10,7 @@ var mysqlConnection = mysql.createConnection({
   user: "user",
   password: "1234",
   database: "productService",
-  multipleStatements: true
+  multipleStatements: true,
 });
 
 mysqlConnection.connect((error) => {
@@ -24,7 +24,7 @@ mysqlConnection.connect((error) => {
 app.listen(3005, () => console.log("Express server at port 3005"));
 
 //Get all product
-app.get("/product/list", (request,response) => {
+app.get("/product/list", (request, response) => {
   mysqlConnection.query("SELECT * from product", (error, rows, field) => {
     if (!error) {
       //console.log(rows);
@@ -36,48 +36,71 @@ app.get("/product/list", (request,response) => {
 });
 
 function e1() {
-    var u='',i=0;
-    while(i++<36) {
-        var c='xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'[i-1],r=Math.random()*16|0,v=c=='x'?r:(r&0x3|0x8);
-        u+=(c=='-'||c=='4')?c:v.toString(16)
-    }
-    return u;
+  var u = "",
+    i = 0;
+  while (i++ < 36) {
+    var c = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx"[i - 1],
+      r = (Math.random() * 16) | 0,
+      v = c == "x" ? r : (r & 0x3) | 0x8;
+    u += c == "-" || c == "4" ? c : v.toString(16);
+  }
+  return u;
 }
-console.log(e1())
+//console.log(e1());
 
 //Delete a product
-app.delete("/product/remove/:id", (request,response) => {
-  mysqlConnection.query("DELETE from product where id=?",[request.params.id], (error, rows, field) => {
-    if (!error) {
-      //console.log(rows);
-      response.send("Deleted Successfully");
-    } else {
-      console.log(error);
+app.delete("/product/remove/:id", (request, response) => {
+  mysqlConnection.query(
+    "DELETE from product where id=?",
+    [request.params.id],
+    (error, rows, field) => {
+      if (!error) {
+        //console.log(rows);
+        response.send("Deleted Successfully");
+      } else {
+        console.log(error);
+      }
     }
-  });
+  );
 });
 
 //post
-app.post('/product/add', (req, res) => {
+app.post("/product/add", (req, res) => {
   let productinput = req.body;
   var guid = e1();
-  var sql = "SET @name = ?;SET @categoryId = ?;SET @guid=?;\
+  var sql =
+    "SET @name = ?;SET @categoryId = ?;SET @guid=?;\
   CALL productAdd(@guid, @name, @categoryId, @output);\
   select @output";
-  mysqlConnection.query(sql, [productinput.name,  productinput.categoryId, guid], (error, rows, fields) => {
+  mysqlConnection.query(
+    sql,
+    [productinput.name, productinput.categoryId, guid],
+    (error, rows, fields) => {
       //console.log(rows[4][0]['@output'])
-      let status=rows[4][0]['@output']
-      if (status=='YES'){
-          res.status(400).send("Duplicate product name")
+      let status = rows[4][0]["@output"];
+      if (status == "YES") {
+        res.status(400).send("Duplicate product name");
+      } else {
+        if (!error) {
+          //console.log(rows);
+          res.status(201).send("Added Successfully");
+        } else res.status(500).send(error);
       }
-      else{
-          if (!error) {
-              //console.log(rows);
-              res.status(201).send("Added Successfully");
-          }
-          else
-              res.status(500).send(error);
-          }
-  })
+    }
+  );
 });
-        
+
+//update product category
+app.put("/product/updateCategory", (req, res) => {
+  let productinput = req.body;
+  var sql ="SET @id=?; SET @categoryId = ?; \
+  CALL categoryUpdate(@id,@categoryId);";
+  mysqlConnection.query(sql,[productinput.id, productinput.categoryId],(err, rows, fields) => 
+  {
+      if (!err) 
+        res.send("Updated successfully");
+      else 
+        console.log(err);
+    }
+  );
+});
